38da809f7a4cfad5f740e484133ed06f26d06d32
pacofeng <pacofeng86@gmail.com>
2022-12-03 14:12:06 -0800

    处理图片资源

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,15 @@ module.exports = {
         test: /\.styl$/,
         use: ['style-loader', 'css-loader', 'stylus-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif|webp)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 50 * 1024, // 小于50kb的图片会被base64处理， 优点：减少请求数量，缺点：体积变得更大
+          },
+        },
+      },
     ],
   },
   // 插件
