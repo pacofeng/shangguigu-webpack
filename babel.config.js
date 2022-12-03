8134a81be26301e628d659ff65f90a026d2fc01f
pacofeng <pacofeng86@gmail.com>
2022-12-03 14:58:39 -0800

    babel使用

---
+++ babel.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // 一个智能预设，允许您使用最新的 JavaScript
+  presets: ['@babel/preset-env'],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -64,6 +64,11 @@ module.exports = {
           filename: 'static/media/[hash:8][ext][query]',
         },
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/, // 排除node_modules代码不编译
+        loader: 'babel-loader',
+      },
     ],
   },
   // 插件
