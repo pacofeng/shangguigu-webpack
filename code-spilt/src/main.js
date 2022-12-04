b0941cf040c8c4631c85d5306b56f9d62861ba39
pacofeng <pacofeng86@gmail.com>
2022-12-04 00:34:21 -0800

    高级-多入口按需加载

--- code-spilt/src/main.js
+++ code-spilt/src/main.js
@@ -1,3 +1,15 @@
 import { add } from './math';
 console.log('main');
 console.log(add(1, 2));
+
+document.getElementById('btn').onclick = function () {
+  // 动态导入 --> 实现按需加载
+  // 即使只被引用了一次，也会代码分割
+  import('./count.js')
+    .then((res) => {
+      console.log('loaded count.js', res.default);
+    })
+    .catch((error) => {
+      console.log('error: ' + error);
+    });
+};
